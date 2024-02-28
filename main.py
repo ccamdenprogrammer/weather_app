13e041eaba2ce6d38dc63ac76bb830dd8daeb8c2
ccamdenprogrammer <ccamden.programmer@gmail.com>
2024-02-27 22:24:00 -0600

    prints out basic weather data instead of a messy pprint json

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ def getWeatherData():
     g = geocoder.ip('me') #Gets lat and long of users current location utilizing geopy 
     lat, lng = g.latlng 
     latlng = (lat, lng)
-    print(latlng)
-    
     from geopy.geocoders import Nominatim # converts lat and long into a more readable city name
     geolocator = Nominatim(user_agent="http")
     location = geolocator.reverse(latlng)
@@ -22,7 +20,20 @@ def getWeatherData():
     base_URL = "http://api.openweathermap.org/data/2.5/weather?appid="+api_key+"&q="+city #requests the API to return data for users current city
     weather_data = requests.get(base_URL).json()
 
-    pprint(weather_data) #prints weather data
+    temp_kelvin = weather_data['main']['temp']
+    temp_celsius, temp_fahrenheit = kelvin_to_CF(temp_kelvin)
+    feels_like_kelvin = weather_data['main']['feels_like']
+    feels_like_celsius, feels_like_fahrenheit =kelvin_to_CF(feels_like_kelvin)
+    description = weather_data['weather'][0]['description']
+
+    print(f"The temperature in {city}: {temp_celsius:.2f} Degrees C / {temp_fahrenheit:.2f} Degrees F. \n")
+    print(f"The temperature in {city} feels like: {feels_like_celsius:.2f} Degrees C / {feels_like_fahrenheit:.2f} Degrees F. \n")
+    print(f"The weather in {city} is: {description}. \n")
+
+def kelvin_to_CF(kelvin):
+    celsius = kelvin-273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
    
 
 getWeatherData()
